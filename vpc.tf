6ac03f8008a337f310f463c86382aad2df533bb7
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-12-22 17:57:31 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "vpc" {
-  cidr_block       = "172.32.0.0/16"
+  cidr_block       = "172.33.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "terraform-test-vpc-1222"
+    Name = "terraform-test-vpc-1223"
   }
 }
