5edcc7c04bbca0a6078439b8d0460c7d4eb299a7
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-12-22 15:46:23 +0900

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "test" {
   bucket = "jhp0204-tf-test-bucket"
   acl    = "private"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "jhp0204-bucket"
+    Environment = "test"
   }
 }
